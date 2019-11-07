80d4301b58c5a916022bfd48fed369249ca5d691
Andrew Wilk <andrewwilk@modv-ve703-11013.apn.wlan.private.upenn.edu>
2019-11-07 12:43:05 -0500

    added comments to lookup view

--- sql_buildfiles/adpsid_views.sql
+++ sql_buildfiles/adpsid_views.sql
@@ -3,7 +3,7 @@
 	/*create lookup view to be able to filter ids by identifier code for cohort*/
 
 		CREATE VIEW lookup AS
-		SELECT generated_ids.id AS id, adsp_id, site_fam_id, site_indiv_id, identifier_code, site_combined_id, adsp_family_id, adsp_indiv_partial_id
+		SELECT generated_ids.id AS id, adsp_id, site_fam_id, site_indiv_id, identifier_code, site_combined_id, adsp_family_id, adsp_indiv_partial_id, comments
 		FROM generated_ids
 		JOIN cohort_identifier_codes
 		ON generated_ids.cohort_identifier_code = cohort_identifier_codes.id WHERE "valid" = TRUE;
