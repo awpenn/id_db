f9b8d4e40ccfb60738ea31ef5febfad3c1c67cc8
Andrew Wilk <andrewwilk@modv-ve703-15427.apn.wlan.private.upenn.edu>
2019-12-09 09:58:25 -0500

    added C cohort codes and added functionality to account for C, G, and A ID prefixes.

--- adspid-csv.py
+++ adspid-csv.py
@@ -11,8 +11,9 @@ new_records = []
 success_id_log = []
 error_log = {}
 special_cohorts = ['LOAD', 'RAS', 'UPN'] #change to correct codes for production
-g_cohorts = ["KGAD", "NIMH", "ADNI", "CCS"]
-load_file = "test.csv"
+g_cohorts = ['KGAD', 'NIMH', 'ADNI', 'CCS']
+c_cohorts = ['ARIC', 'ASPS', 'CHS', 'ERF', 'FHS', 'RS']
+load_file = 'test.csv'
 family_data_creation = False
 
 def main():
@@ -24,7 +25,7 @@ def main():
     DBUSER = os.getenv('DBUSER')
     global family_data_creation
     
-    select_casefam = input("Are you loading family data? (y/n)")
+    select_casefam = input('Are you loading family data? (y/n)')
     if select_casefam not in ['y', 'n', 'Y', 'N', 'yes', 'no', 'YES', 'NO', 'Yes', 'No']:
         main()
 
@@ -56,7 +57,7 @@ def connect_database(DBIP, DBPASS, DBPORT, DB, DBUSER):
                     )
 
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM lookup")
+        cursor.execute('SELECT * FROM lookup')
 
         current_records = cursor.fetchall()
         create_dict()
@@ -150,11 +151,11 @@ def legacy_check(legacy_check_dict, callback):
 
         else:
             if len(returned_cohort_code_tuple) > 1:
-                print(f"More than one cohort found associated with {key}. Selecting for legacy criteria. Please check database after upload to ensure accuracy.")
+                print(f'More than one cohort found associated with {key}. Selecting for legacy criteria. Please check database after upload to ensure accuracy.')
                         
-                error_log[key] = [value, "More than one cohort found for subject in legacy check. Writing with one selected from legacy cohorts, but check database to ensure accuracy."]
+                error_log[key] = [value, 'More than one cohort found for subject in legacy check. Writing with one selected from legacy cohorts, but check database to ensure accuracy.']
                         
-                print("Associated cohort codes found:")
+                print('Associated cohort codes found:')
                 for var in returned_cohort_code_tuple:
                     print(var[0])
 
@@ -163,12 +164,12 @@ def legacy_check(legacy_check_dict, callback):
                         processed_legacy_dict[f'{returned_cohort_code}-{lookup_id}'] = [query_family_id, site_indiv_id, lookup_id, returned_cohort_code]
                     
                     else:
-                        error_log[key] = [value, "More than one cohort found for subject in legacy check, and none found matching legacy conditions. Check your loadfile and the database."]
+                        error_log[key] = [value, 'More than one cohort found for subject in legacy check, and none found matching legacy conditions. Check your loadfile and the database.']
                         break
             else:
                 print(f'No associated cohort was found for {key}.  NIALOAD has been assigned, but check the database and log to assure correctness.')
                 returned_cohort_code = 'LOAD'
-                error_log[key] = [value, "No cohort was found for the family of this subject, suggesting it is new. NIALOAD was assigned, but check for correctness."]
+                error_log[key] = [value, 'No cohort was found for the family of this subject, suggesting it is new. NIALOAD was assigned, but check for correctness.']
 
 
         processed_legacy_dict[f'{returned_cohort_code}-{lookup_id}'] = [query_family_id, site_indiv_id, lookup_id, returned_cohort_code]
@@ -183,7 +184,7 @@ def compare(current_records_dict, new_records_dict):
             if current_records_dict[key]:
                 print(f'record exists for: {current_records_dict[key]}')
 
-                error_log[key] = [value, "A record for this subject already exists in the database. Check the database and your loadfile for correctness."]
+                error_log[key] = [value, 'A record for this subject already exists in the database. Check the database and your loadfile for correctness.']
 
         except:
             print(f'new record will be created for {key}')
@@ -211,10 +212,13 @@ def write_to_database(records_to_database_dict):
 
         cohort_identifier_code = value[3]
 
-        if cohort_identifier_code in g_cohorts:
-            id_prefix = "G"
+        if cohort_identifier_code in g_cohorts or cohort_identifier_code in c_cohorts:
+            if cohort_identifier_code in g_cohorts:
+                id_prefix = 'G'
+            else:
+                id_prefix = 'C'
         else:
-            id_prefix = "A"
+            id_prefix = 'A'
 
         ## initialized as 0, will change if fam is switched on
         adsp_family_id = 0
@@ -235,8 +239,8 @@ def write_to_database(records_to_database_dict):
                     adsp_family_id = row[0]   
             else:
                 print(f'there seems to be no adsp_family_id found associated with site family id {site_fam_id}. Please check the database')
-                # error_log[key] = [value, "No adsp_family_id was found for this subject's site_family_id"]
-                make_fam_id = input("Do you want to generate a new ADSP_family_id for this site_family_id?(y/n)")
+                # error_log[key] = [value, 'No adsp_family_id was found for this subject's site_family_id']
+                make_fam_id = input('Do you want to generate a new ADSP_family_id for this site_family_id?(y/n)')
                 if(make_fam_id == 'y'):
                     print(f'making fam id, finding last made family id for {cohort_identifier_code}')
                     cursor.execute(f"SELECT adsp_family_id FROM lookup WHERE cohort_identifier_code = '{cohort_identifier_code}' AND adsp_family_id IS NOT NULL ORDER BY adsp_family_id DESC LIMIT 1")
@@ -244,7 +248,7 @@ def write_to_database(records_to_database_dict):
                     _code
                     if len(retrieved_adsp_family) < 1:
                         print(retrieved_adsp_family)
-                        error_log[key] = [value, "Error: Attempted to create new adsp_family_id, but no adsp_family_ids found associated with this cohort."]
+                        error_log[key] = [value, 'Error: Attempted to create new adsp_family_id, but no adsp_family_ids found associated with this cohort.']
                         continue
                     else:
                         print(f'An adsp_family_id was returned as last made for {cohort_identifier_code}')
@@ -266,9 +270,9 @@ def write_to_database(records_to_database_dict):
         retrieved_partial = cursor.fetchall()
 
         if len(retrieved_partial) < 1:
-            print(f"No adsp_indiv_partial found associated with {cohort_identifier_code}.  Check that you're using the correct cohort identifier code (letter-based). No record will be created.")
+            print(f'No adsp_indiv_partial found associated with {cohort_identifier_code}.  Check that you're using the correct cohort identifier code (letter-based). No record will be created.')
             
-            error_log[key] = [value, f"Error: No indiv_partial was returned when queried for cohort code: {cohort_identifier_code}. Check that letter code and not table key is in loadfile."]
+            error_log[key] = [value, f'Error: No indiv_partial was returned when queried for cohort code: {cohort_identifier_code}. Check that letter code and not table key is in loadfile.']
         else:
 
             for row in retrieved_partial:
@@ -287,10 +291,10 @@ def write_to_database(records_to_database_dict):
 
 def generate_errorlog():
     timestamp = calendar.timegm(time.gmtime())
-    f = open(f"./log_files/{timestamp}-log.txt", "w+")
-    f.write(f"{str(len(error_log.items()))} flag(s) raised in runtime. See details below: \n\n")
+    f = open(f'./log_files/{timestamp}-log.txt', 'w+')
+    f.write(f'{str(len(error_log.items()))} flag(s) raised in runtime. See details below: \n\n')
     for key, value in error_log.items():
-        f.write(f"Error: {value[1]} \n")
+        f.write(f'Error: {value[1]} \n')
         f.write(f'family_site_id: {value[0][0]}\n')
         f.write(f'indiv_site_id: {value[0][1]}\n')
         f.write(f'combined_site_id: {value[0][2]}\n')
@@ -298,14 +302,14 @@ def generate_errorlog():
 
 def generate_success_list():
     timestamp = calendar.timegm(time.gmtime())
-    f = open(f"./log_files/success_lists/{timestamp}-generated_ids.txt", "w+")
+    f = open(f'./log_files/success_lists/{timestamp}-generated_ids.txt', 'w+')
     for id in success_id_log:
         if success_id_log.index(id) >= len(success_id_log)-1:
             f.write(id)
         else:
-            f.write(id + ", ")
+            f.write(id + ', ')
 
     f.close()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
