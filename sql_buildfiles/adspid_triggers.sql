73d78845d2959429be86ce27714858af0be8d0ee
Andrew Wilk <andrewwilk@client0645vag.vpn.private.upenn.edu>
2020-09-24 13:18:51 -0400

    generalized cleanup nulls trigger function

--- sql_buildfiles/adspid_triggers.sql
+++ sql_buildfiles/adspid_triggers.sql
@@ -6,10 +6,12 @@ CREATE OR REPLACE FUNCTION cleanup_nulls()
 		$cleanup_nulls_trigger$
         DECLARE
         i RECORD;
+		_table_name TEXT;
         BEGIN
-			for i in (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'generated_ids' AND data_type = 'character varying')
+		_table_name := TG_ARGV[0];
+			for i in EXECUTE format('SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = $1 AND data_type = $2') USING _table_name, 'character varying'
 				LOOP
-					EXECUTE format('UPDATE generated_ids SET %I = NULL WHERE %I = $1', i.column_name, i.column_name) USING 'NULL';
+					EXECUTE format('UPDATE %I SET %I = NULL WHERE %I = $1', _table_name, i.column_name, i.column_name) USING 'NULL';
 				END LOOP;
 				RETURN NULL;
         END
@@ -23,4 +25,4 @@ LANGUAGE plpgsql;
 CREATE TRIGGER cleanup_nulls_trigger AFTER INSERT ON generated_ids -- if you make it AFTER UPDATE will loop. 
 -- either look into that query-depth thing from consent_base(?) or leave as INSERT(?)
 	FOR EACH STATEMENT
-	EXECUTE PROCEDURE cleanup_nulls();
+	EXECUTE PROCEDURE cleanup_nulls("generated_ids");
